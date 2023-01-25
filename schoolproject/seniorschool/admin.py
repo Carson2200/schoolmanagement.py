32ee0ca42a00be5f6f9b5ca3aef383f724d96ee0
carson2200 <carsonosore78@gmail.com>
2023-01-25 15:53:10 +0300

    Added Table view

--- schoolproject/seniorschool/admin.py
+++ schoolproject/seniorschool/admin.py
@@ -3,8 +3,25 @@ from .models import Stream, Teacher, Subject, Student
 
 # Register your models here.
 
+class StreamTable(admin.ModelAdmin):
+    list_display = ('Stream_Name', 'Stream_Name')
 
-admin.site.register(Stream)
-admin.site.register(Teacher)
-admin.site.register(Subject)
-admin.site.register(Student)
+admin.site.register(Stream, StreamTable)
+
+class Teachertabe(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone_number', 'profile_pic', 'Stream','Subject')
+admin.site.register(Teacher, Teachertabe)
+
+
+class SubjectTable(admin.ModelAdmin):
+    list_display = ('Subject_Code','Subject_Name')
+
+admin.site.register(Subject, SubjectTable)
+
+
+
+class StudentTable(admin.ModelAdmin):
+    list_display = ('Admission_no','first_name','last_name','dob','Parent_phone_number',
+                'profile_pic','age','location', 'date_entered','last_updated','Stream' )
+
+admin.site.register(Student, StudentTable)
